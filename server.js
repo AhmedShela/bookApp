ffc8e71406dd9972462398d73c4a0e26d6609146
ahmed <ahmedsh717@gmail.com>
2020-07-12 14:36:54 +0300

    setUp Files

---
+++ server.js
@@ -0,0 +1,17 @@
+'use strict';
+require('dotenv').config()
+const express = require('express');
+const server = express();
+const PORT = process.env.PORT;
+
+server.use(express.static('./public'));
+
+server.use(express.json());
+server.use(express.urlencoded({extended:true}));
+
+server.set('view engine','ejs');
+
+
+server.listen(PORT,() =>{
+console.log(`listening to port : ${PORT}`);
+});
