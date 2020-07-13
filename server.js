adc5ef1c076cf47d989f051d56971c9eb05eabcb
ahmed <ahmedsh717@gmail.com>
2020-07-13 14:32:20 +0300

    lab-11

--- server.js
+++ server.js
@@ -4,30 +4,16 @@ const express = require('express');
 const server = express();
 const PORT = process.env.PORT || 3030;
 const agent = require('superagent')
-server.use(express.static('./public'));
 
+server.use(express.static('./public'));
 server.use(express.json());
 server.use(express.urlencoded({extended:true}));
 
 server.set('view engine','ejs');
 
-server.get('/hello',(req,res)=>{
+server.get('/',(req,res)=>{
     res.render('./pages/index.ejs');
 });
-
-// server.get('/searches/show',(req,res)=>{
-// res.render('./pages/searches/show.ejs');
-// });
-
-// server.get('/searches',(req,res)=>{
-//     let url = `https://www.googleapis.com/books/v1/volumes?q=dogs`;
-//     superagent.get(url)
-//     .then (dogsData =>{
-//         console.log(dogsData.body.items)
-//         res.render('booksData',{dogData: dogsData.body });
-//     })
-// })
-// https://www.googleapis.com/books/v1/volumes?q=search+terms
 
 server.post('/searches',(req,res)=>{
     var searchKey = req.body.bookSearch;
@@ -41,31 +27,28 @@ server.post('/searches',(req,res)=>{
 
 agent.get(url)
 .then(bookData =>{
-    // console.log(bookData.body.items[0].volumeInfo.authors[0]);
-
     let result = bookData.body.items.map(element =>{
         return new Book(element)
     });
-    // res.status(200).send(bookData.body.items[0]);
-    // res.render('books',{booksResult: result})
-    res.render('pages/searches/show',{booksResult: result });
+    res.render('./pages/searches/show',{booksResult: result });
 
-})
-})
+});
+});
 
 server.get('/search/new',(req,res)=>{
-    res.render('pages/searches/new.ejs')
-})
-// https://i.imgur.com/J5LVHEL.jpg
+    res.render('./pages/searches/new.ejs')
+});
+
 function Book(data) {
     this.bookName = data.volumeInfo.title,
     this.bookAuthor = ((data.volumeInfo.authors) ? data.volumeInfo.authors[0] : 'unKnown')//data.volumeInfo.authors[0],
     this.bookDesc = data.volumeInfo.description,
-    this.bookImage = ((data.volumeInfo.imageLinks.thumbnail) ? data.volumeInfo.imageLinks.thumbnail : 'https://i.imgur.com/J5LVHEL.jpg')
-}
-server.get('',(req,res)=>{
-    
-})
+    this.bookImage = ((data.volumeInfo.imageLinks) ? data.volumeInfo.imageLinks.thumbnail : 'https://i.imgur.com/J5LVHEL.jpg')
+};
+
+server.get('/*',(req,res)=>{
+    res.render('pages/error.ejs')
+});
 server.listen(PORT,() =>{
 console.log(`listening to port : ${PORT}`);
 });
